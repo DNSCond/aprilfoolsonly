42243cb2fa55d2ae6c40dec49a75b9a1b2ff71b3
qin2007 <qin@ractoc.com>
2026-01-08 22:51:33 +0100

    private

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 a joke specifically written for r/drawmyoc
 
+[github: https://github.com/DNSCond/aprilfoolsonly](https://github.com/DNSCond/aprilfoolsonly),
+[devvit app](https://developers.reddit.com/apps/aprilfoolsonly).
+
+[for an actual public app use this](https://developers.reddit.com/apps/randomposter-app).
--- src/main.ts
+++ src/main.ts
@@ -1,130 +1,71 @@
-import { Devvit, type FormField } from "@devvit/public-api";
+// Visit developers.reddit.com/docs to learn Devvit!
+import { Devvit } from '@devvit/public-api';
 
-import { handleNuke, handleNukePost } from "./nuke.js";
-
-Devvit.configure({
-  redditAPI: true,
-});
-
-const nukeFields: FormField[] = [
+Devvit.configure({ redditAPI: true, });
+const defaultValue = 'Post does not contain an image or gallery';
+Devvit.addSettings([
   {
-    name: "remove",
-    label: "Remove comments",
     type: "boolean",
-    defaultValue: true,
-  },
-  {
-    name: "lock",
-    label: "Lock comments",
-    type: "boolean",
+    name: 'reportWithoutImage',
+    label: 'report posts without image?',
+    helpText: 'crossposts and textposts with image will be reported as well. this was the original functionallity',
     defaultValue: false,
   },
   {
-    name: "skipDistinguished",
-    label: "Skip distinguished comments",
-    type: "boolean",
-    defaultValue: false,
+    type: "string",
+    name: 'onlyIfFlairId',
+    label: 'flair_template_id?',
+    defaultValue: '',
   },
-] as const;
-
-const nukeForm = Devvit.createForm(
-  () => {
-    return {
-      fields: nukeFields,
-      title: "Mop Comments",
-      acceptLabel: "Mop",
-      cancelLabel: "Cancel",
-    };
-  },
-  async ({ values }, context) => {
-    if (!values.lock && !values.remove) {
-      context.ui.showToast("You must select either lock or remove.");
-      return;
-    }
-
-    if (context.commentId) {
-      const result = await handleNuke(
-        {
-          remove: values.remove,
-          lock: values.lock,
-          skipDistinguished: values.skipDistinguished,
-          commentId: context.commentId,
-          subredditId: context.subredditId,
-        },
-        context
-      );
-      console.log(
-        `Mop result - ${result.success ? "success" : "fail"} - ${
-          result.message
-        }`
-      );
-      context.ui.showToast(
-        `${result.success ? "Success" : "Failed"} : ${result.message}`
-      );
-    } else {
-      context.ui.showToast(`Mop failed! Please try again later.`);
-    }
+  {
+    type: "string",
+    name: 'report_reason',
+    label: 'Report Reason?',
+    helpText: 'only 3 to 88 characters',
+    onValidate({ value }) {
+      const len = BigInt(value?.length || 0);
+      if (len < +3) return `${RangeError('Character count must be more than 3')}`;
+      if (len > 88) return `${RangeError('Character count must be less than 88')}`;
+    }, defaultValue,
   }
-);
+]);
 
-Devvit.addMenuItem({
-  label: "Mop comments",
-  description:
-    "Remove this comment and all child comments. This might take a few seconds to run.",
-  location: "comment",
-  forUserType: "moderator",
-  onPress: (_, context) => {
-    context.ui.showForm(nukeForm);
-  },
-});
+Devvit.addTrigger({
+  event: 'PostCreate',
+  async onEvent(event, context) {
+    if (await context.settings.get('reportWithoutImage')) {
+      const date = new Date(event.post?.createdAt || new Date), currentFullYear = (new Date).getFullYear();
+      if (!isInDateRange(date, Date.UTC(currentFullYear, 3), Date.UTC(currentFullYear, 3, 2))) return;
+      const toMatch = await context.settings.get<string>('onlyIfFlairId');
+      if (event.post?.linkFlair?.templateId !== toMatch && toMatch) {
+        return;
+      }
 
-const nukePostForm = Devvit.createForm(
-  () => {
-    return {
-      fields: nukeFields,
-      title: "Mop Post Comments",
-      acceptLabel: "Mop",
-      cancelLabel: "Cancel",
-    };
-  },
-  async ({ values }, context) => {
-    if (!values.lock && !values.remove) {
-      context.ui.showToast("You must select either lock or remove.");
-      return;
+      // event.post contains the post data
+      if (event.post?.id && !event.post?.isImage && !event.post?.isGallery) {
+        const reason = `u/${context.appName}: ` + ((await context.settings.get<string>('report_reason')) || defaultValue);
+        // Report the post if it does not contain an image
+        const reportablePost = await context.reddit.getPostById(event.post.id);
+        await context.reddit.report(reportablePost, { reason });
+      }
     }
-
-    if (!context.postId) {
-      throw new Error("No post ID");
-    }
-
-    const result = await handleNukePost(
-      {
-        remove: values.remove,
-        lock: values.lock,
-        skipDistinguished: values.skipDistinguished,
-        postId: context.postId,
-        subredditId: context.subredditId,
-      },
-      context
-    );
-    console.log(
-      `Mop result - ${result.success ? "success" : "fail"} - ${result.message}`
-    );
-    context.ui.showToast(
-      `${result.success ? "Success" : "Failed"} : ${result.message}`
-    );
-  }
-);
-
-Devvit.addMenuItem({
-  label: "Mop post comments",
-  description:
-    "Remove all comments of this post. This might take a few seconds to run.",
-  location: "post",
-  forUserType: "moderator",
-  onPress: (_, context) => {
-    context.ui.showForm(nukePostForm);
   },
 });
 
 export default Devvit;
+function isInDateRange(
+  needle: Date | string | number,
+  start: Date | string | number,
+  end: Date | string | number,
+): boolean {
+  const
+    needle_: Date = new Date(needle),
+    start_: Date = new Date(start),
+    end_: Date = new Date(end);
+  if (needle_ < start_) return false;
+  if (needle_ > end_) return false;
+  // @ts-expect-error
+  if (isNaN(needle_) || isNaN(start_) || isNaN(end_))
+    return false;
+  return true;
+}
