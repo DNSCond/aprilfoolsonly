43fdb9d4a69f0937ba05bae5561ecf464cb9fabc
qin2007 <qin@ractoc.com>
2026-04-01 15:42:01 +0200

    simplicifation

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,10 @@ const defaultValue = 'Post does not contain an image or gallery';
 Devvit.addSettings([
   {
     type: "boolean",
-    name: 'reportWithoutImage',
-    label: 'report posts without image?',
-    helpText: 'crossposts and textposts with image will be reported as well. this was the original functionallity',
+    name: 'acticve',
+    label: 'is Active?',
+    helpText: 'report posts on april 1 UTC?',
     defaultValue: false,
-  },
-  {
-    type: "string",
-    name: 'onlyIfFlairId',
-    label: 'flair_template_id?',
-    defaultValue: '',
   },
   {
     type: "string",
@@ -34,17 +28,11 @@ Devvit.addTrigger({
   event: 'PostCreate',
   async onEvent(event, context) {
     if (await context.settings.get('reportWithoutImage')) {
-      const date = new Date(event.post?.createdAt || new Date), currentFullYear = (new Date).getFullYear();
-      if (!isInDateRange(date, Date.UTC(currentFullYear, 3), Date.UTC(currentFullYear, 3, 2))) return;
-      const toMatch = await context.settings.get<string>('onlyIfFlairId');
-      if (event.post?.linkFlair?.templateId !== toMatch && toMatch) {
-        return;
-      }
-
-      // event.post contains the post data
-      if (event.post?.id && !event.post?.isImage && !event.post?.isGallery) {
+      const date = new Date(event.post?.createdAt || NaN), currentDate = new Date(Date.UTC(2026,3));
+      if (currentDate.setUTCHours(0, 0, 0, 0) !== date.setUTCHours(0, 0, 0, 0)) return;
+      // const toMatch = await context.settings.get<string>('onlyIfFlairId');
+      if (event.post) {
         const reason = `u/${context.appName}: ` + ((await context.settings.get<string>('report_reason')) || defaultValue);
-        // Report the post if it does not contain an image
         const reportablePost = await context.reddit.getPostById(event.post.id);
         await context.reddit.report(reportablePost, { reason });
       }
